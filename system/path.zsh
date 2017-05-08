0851a6665b71b47562796c903398566acc10b134
dubuqingfeng <1135326346@qq.com>
2017-05-08 11:00:37 +0800

    update system path

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:${HOME}/bin/:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Tex/Root/bin/x86_64-darwiin:$GOPATH/bin"
+export PATH="/Users/zhengyan.gao/.composer/vendor/bin:/usr/local/opt/coreutils/libexec/gnubin:${HOME}/bin/:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Tex/Root/bin/x86_64-darwiin:$GOPATH/bin"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
