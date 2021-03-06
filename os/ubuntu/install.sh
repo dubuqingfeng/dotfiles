8512ccca8a928b8e44cc0d18c927e4c34eb103d0
dubuqingfeng <1135326346@qq.com>
2021-03-06 13:50:15 +0800

    add typora for ubuntu

--- os/ubuntu/install.sh
+++ os/ubuntu/install.sh
@@ -4,83 +4,80 @@
 #
 
 echo "Updating Packages."
-
 apt-get -qq update
-
 echo "Installing Software."
 echo "Installing Core Packages."
 
-#ldconfig
-if [ $(getconf WORD_BIT) = '32' ] && [ $(getconf LONG_BIT) = '64' ] ; then
-    echo "64"
-    #sogou pinyin
-    #wget -q -O sogoupinyin_i386.deb http://pinyin.sogou.com/linux/download.php?f=linux&bit=64
-else
-    echo "32"
-fi
+# 必备软件
+necessary=(
+    vim
+    git
+    zsh
+    python-pip
+    pm-utils
+    indicator-muiltload
+)
 
-#Vim
-apt-get install -qq -y vim
-#Git
-apt-get install -qq -y git
-#gitg
-apt-get install -qq -y gitg
-#Zsh
-apt-get install -qq -y zsh
+app=(
+    gitg
+    # keepasss
+    # terminator
+    # vscode
+)
+
+binaries=(
+    # docker
+    # nodejs
+    # proxychains-ng
+    # vi /usr/local/etc/proxychains.conf
+    # sogoupinyin
+    # wget -q -O sogoupinyin_i386.deb http://pinyin.sogou.com/linux/download.php?f=linux&bit=64
+    # java
+    # tar -xzf jdk-8u65-linux-x64.tar.gz
+    # mv jdk1.8.0_101 /usr/local/java
+    #chrome
+    # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    # dpkg -i google-chrome-stable_current_amd64.deb
+    # apt-get install -f
+    ###
+)
+
+repositories=(
+    ppa:hzwhuang/ss-qt5
+    ppa:webupd8team/sublime-text-3
+    ppa:docky-core/stable
+    ppa:damien-moore/codeblocks-stable
+    ppa:freyja-dev/unity-tweak-tool-daily
+)
+
+dep_app=(
+    shadowsocks-qt5
+    sublime-text-installer
+    codeblocks
+    # docky
+    # unity-tweak-tool
+)
+
+apt-get install -qq -y ${necessary[@]}
+
+echo "Installing oh my zsh..."
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-#Python-pip
-apt-get install -qq -y python-pip
-#terminator
-apt-get install -qq -y terminator
-#java
-#tar -xzf jdk-8u65-linux-x64.tar.gz
-#mv jdk1.8.0_101 /usr/local/java
 
-add-apt-repository -y ppa:fcitx-team/nightly
-add-apt-repository -y ppa:hzwhuang/ss-qt5
-add-apt-repository -y ppa:webupd8team/sublime-text-3
-#Docky
-add-apt-repository -y ppa:docky-core/stable
-#Unity-tweak-tool-daily
-add-apt-repository -y ppa:freyja-dev/unity-tweak-tool-daily
-#Code::blocks
-add-apt-repository -y ppa:damien-moore/codeblocks-stable
+echo "Installing Repositories..."
 
-###
-#chrome
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#dpkg -i google-chrome-stable_current_amd64.deb
-#apt-get install -f
+add-apt-repository -y ${repositories[@]}
+apt-get install -qq -y ${dep_app[@]}
 
-# apt-get install -qq -y php7.0 libapache2-mod-php7.0 php7.0-gd php-pear php7.0-mbstring
+install_typora() {
+    # sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+    wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+    # add Typora's repository
+    add-apt-repository 'deb https://typora.io/linux ./'
+    apt-get update
+    # install typora
+    apt-get install typora
+}
 
-
-###
-apt-get install -qq -y fcitx
-apt-get install -qq -y shadowsocks-qt5
-apt-get install -qq -y sublime-text-installer
-
-# apt-get install -qq -y docky
-# apt-get install -qq -y unity-tweak-tool
-apt-get install -qq -y codeblocks
-
-#proxychains-ng
-#vi /usr/local/etc/proxychains.conf
-
-#composer
-#curl -sS https://getcomposer.org/installer | php
-#mv composer.phar /usr/local/bin/composer
-#chmod 755 /usr/local/bin/composer
-#docker
-#vagrant
-#
-#apache2
-#nodejs
-#phalcon
-#界面美化
-
-#android / LaTeX
-#keepass
-#deluge
+# apt-get install -qq -y ibus ibus-clutter ibus-gtk ibus-gtk3 ibus-qt4 ibus-pinyin
 
 rm -rf /var/lib/apt/lists/*
