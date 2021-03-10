ef9e7cc5069e908bfffa1ecd68adfc0d7ade77c9
dubuqingfeng <1135326346@qq.com>
2021-03-10 14:55:49 +0800

    update path.

--- pkg/go/path.zsh
+++ pkg/go/path.zsh
@@ -1,3 +1,9 @@
+if [ -d /usr/local/opt/go/libexec ]; then
+    export GOROOT=/usr/local/opt/go/libexec
+fi
+if [ -d /usr/local/go ]; then
+    export GOROOT=/usr/local/go
+fi
 export GOROOT=/usr/local/opt/go/libexec
 export GOPATH=$HOME/.go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:/usr/local/go/bin
+export PATH="$PATH:${GOROOT}/bin:${GOPATH}/bin:/usr/local/go/bin"
--- pkg/system/path.zsh
+++ pkg/system/path.zsh
@@ -1,6 +1,7 @@
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:${HOME}/bin/:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Tex/Root/bin/x86_64-darwiin:/usr/local/bin"
+export PATH="$PATH:/usr/local/opt/coreutils/libexec/gnubin:${HOME}/bin/:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Tex/Root/bin/x86_64-darwiin:/usr/local/bin"
 export PATH="$PATH:${HOME}/.composer/vendor/bin:/usr/local/opt/coreutils/libexec/gnubin:${HOME}/bin/:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Tex/Root/bin/x86_64-darwiin:$GOPATH/bin"
 export PATH="/usr/local/opt/icu4c/bin:/usr/local/opt/icu4c/sbin:$PATH"
+export PATH="/usr/local/opt/libxml2/bin:$PATH"
 export PATH="${HOME}/work/bin:$PATH"
 
 export NVM_DIR="$HOME/.nvm"
