015cd7db0b266287fde807ba099c8e19e366b473
dubuqingfeng <1135326346@qq.com>
2021-03-10 15:03:14 +0800

    update path.

--- pkg/go/path.zsh
+++ pkg/go/path.zsh
@@ -4,6 +4,5 @@ fi
 if [ -d /usr/local/go ]; then
     export GOROOT=/usr/local/go
 fi
-export GOROOT=/usr/local/opt/go/libexec
 export GOPATH=$HOME/.go
 export PATH="$PATH:${GOROOT}/bin:${GOPATH}/bin"
