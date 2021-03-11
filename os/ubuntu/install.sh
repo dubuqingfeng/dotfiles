8c175a5daa3add659542e2c149eda29f3e6becff
dubuqingfeng <1135326346@qq.com>
2021-03-11 16:57:30 +0800

    update files

--- os/ubuntu/install.sh
+++ os/ubuntu/install.sh
@@ -3,74 +3,10 @@
 #
 #
 
-echo "Updating Packages."
-apt-get -qq update
-echo "Installing Software."
-echo "Installing Core Packages."
-
-# 必备软件
-necessary=(
-    vim
-    git
-    zsh
-    python-pip
-    pm-utils
-    htop
-    ibus-pinyin
-)
-
-app=(
-    gitg
-    # keepasss
-    # terminator
-    # 手动安装：
-    # vscode
-    # nutstore
-)
-
-binaries=(
-    # docker
-    # nodejs
-    # proxychains-ng
-    # vi /usr/local/etc/proxychains.conf
-    # sogoupinyin
-    # wget -q -O sogoupinyin_i386.deb http://pinyin.sogou.com/linux/download.php?f=linux&bit=64
-    # java
-    # tar -xzf jdk-8u65-linux-x64.tar.gz
-    # mv jdk1.8.0_101 /usr/local/java
-    #chrome
-    # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    # dpkg -i google-chrome-stable_current_amd64.deb
-    # apt-get install -f
-    ###
-)
-
-repositories=(
-    ppa:hzwhuang/ss-qt5
-    ppa:webupd8team/sublime-text-3
-    ppa:docky-core/stable
-    ppa:damien-moore/codeblocks-stable
-    ppa:freyja-dev/unity-tweak-tool-daily
-)
-
-dep_app=(
-    shadowsocks-qt5
-    sublime-text-installer
-    codeblocks
-    indicator-muiltload
-    # docky
-    # unity-tweak-tool
-)
-
-apt-get install -qq -y ${necessary[@]}
-
-echo "Installing oh my zsh..."
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-
-echo "Installing Repositories..."
-
-# add-apt-repository -y ${repositories[@]}
-# apt-get install -qq -y ${dep_app[@]}
+install_ohmyzsh() {
+    echo "Installing oh my zsh..."
+    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+}
 
 install_typora() {
     # sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
@@ -82,6 +18,70 @@ install_typora() {
     apt-get install typora
 }
 
-# apt-get install -qq -y ibus ibus-clutter ibus-gtk ibus-gtk3 ibus-qt4 ibus-pinyin
+echo "Updating Packages."
+apt-get -qq update
+echo "Installing Software."
+echo "Installing Core Packages."
+
+# 必备软件
+necessary=(
+    vim
+    git
+    zsh
+    pm-utils
+    htop
+    # apt-get install -qq -y ibus ibus-clutter ibus-gtk ibus-gtk3 ibus-qt4 ibus-pinyin
+    ibus-pinyin
+)
+
+app=(
+    gitg
+    # keepasss
+    # terminator
+    # 手动安装：
+    # vscode
+    # nutstore
+    # sublime-text-installer
+    # codeblocks
+    # indicator-muiltload
+)
+
+binaries=(
+    # docker
+    # nodejs
+    # nvm
+    # proxychains-ng
+    # sogoupinyin
+    #   wget -q -O sogoupinyin_i386.deb http://pinyin.sogou.com/linux/download.php?f=linux&bit=64
+    # java
+    #   tar -xzf jdk-8u65-linux-x64.tar.gz
+    #   mv jdk1.8.0_101 /usr/local/java
+    # chrome
+    #   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    #   dpkg -i google-chrome-stable_current_amd64.deb
+    #   apt-get install -f
+    ###
+)
+
+# 需要 add-apt-repository 的，需要注意是否有对应版本
+repositories=(
+)
+
+dep_app=(
+)
+
+apt-get install -qq -y ${necessary[@]}
+echo "Installing Repositories..."
+
+
+for i in $repositories
+  do
+   add-apt-repository -y $i
+done
+apt-get -qq update
+apt-get install -qq -y ${dep_app[@]}
+
+# install_ohmyzsh
+# install_typora
 
 rm -rf /var/lib/apt/lists/*
