61c4a5af2243d38e32f6e72992b66731cb9de04f
dubuqingfeng <1135326346@qq.com>
2021-03-18 16:23:16 +0800

    add ubuntu docker.

--- os/ubuntu/install.sh
+++ os/ubuntu/install.sh
@@ -52,6 +52,9 @@ app=(
 
 binaries=(
     # docker
+    #   curl -fsSL https://get.docker.com -o get-docker.sh
+    #   sudo sh get-docker.sh
+    #   sudo usermod -aG docker <your-user>
     # nodejs
     # nvm
     # proxychains-ng
